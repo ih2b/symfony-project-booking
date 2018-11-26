6da7246070d2be807ed79bbc4fd19d9cd7dfd701
fakherbourray <39460329+fakherbourray@users.noreply.github.com>
2018-11-26 02:02:18 +0100

    Update function.php

--- resource/function.php
+++ resource/function.php
@@ -62,15 +62,17 @@ function display_message(){
 
 function send_message(){
     if(isset($_POST['submit'])){
-        $to = "ghandri.sofiene@gmail.com";
+
+        $to = "fakherbourray@gmail.com";
         $from_name = $_POST['name'];
         $subject = $_POST['subject'];
         $email = $_POST['email'];
         $message = $_POST['message'];
 
-        $header = "From: {$from_name} {$email}";
+        $headers = "From: {$from_name} {$email}";
 
-        $result = mail($to, $subject, $message, $header);
+        $result = mail($to, $subject, $message, $headers);
+        
         if(!$result){
             set_message("try again");
             redirect("contact.php");
