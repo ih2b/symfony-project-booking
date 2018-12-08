81e6132625385a1e38149ab321e6fe78871a5632
iheb1 <mohamed.iheb.khedhri@gmail.com>
2018-12-08 21:23:05 +0100

    register  function

--- public/registerform.php
+++ public/registerform.php
@@ -1,3 +1,4 @@
+<?php require_once("../resource/config.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -27,46 +28,25 @@
                 <div class="card-body">
                     <h2 class="title">Registration Form</h2>
                     <form method="POST">
+                        <?php 
+                        display_message(); 
+                        register_user(); ?>
                         <div class="row row-space">
                             <div class="col-2">
                                 <div class="input-group">
                                     <label class="label">first name</label>
-                                    <input class="input--style-4" type="text" name="first_name">
+                                    <input class="input--style-4" type="text" name="prenom">
                                 </div>
                             </div>
                             <div class="col-2">
                                 <div class="input-group">
                                     <label class="label">last name</label>
-                                    <input class="input--style-4" type="text" name="last_name">
+                                    <input class="input--style-4" type="text" name="nom">
                                 </div>
                             </div>
                         </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Birthday</label>
-                                    <div class="input-group-icon">
-                                        <input class="input--style-4 js-datepicker" type="text" name="birthday">
-                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Gender</label>
-                                    <div class="p-t-10">
-                                        <label class="radio-container m-r-45">Male
-                                            <input type="radio" checked="checked" name="gender">
-                                            <span class="checkmark"></span>
-                                        </label>
-                                        <label class="radio-container">Female
-                                            <input type="radio" name="gender">
-                                            <span class="checkmark"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        
+                     
                         <div class="row row-space">
                             <div class="col-2">
                                 <div class="input-group">
@@ -80,21 +60,17 @@
                                     <input class="input--style-4" type="text" name="phone">
                                 </div>
                             </div>
-                        </div>
-                        <div class="input-group">
-                            <label class="label">Subject</label>
-                            <div class="rs-select2 js-select-simple select--no-search">
-                                <select name="subject">
-                                    <option disabled="disabled" selected="selected">Choose option</option>
-                                    <option>Subject 1</option>
-                                    <option>Subject 2</option>
-                                    <option>Subject 3</option>
-                                </select>
-                                <div class="select-dropdown"></div>
+                         <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">PassWord</label>
+                                   <input class="input--style-4" type="password"  name="txt_upass"  />
+                                </div>
                             </div>
                         </div>
+                       
                         <div class="p-t-15">
-                            <button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
+                            <input type="submit" name="submit" class="btn btn--radius-2 btn--blue"  >
+                            
                         </div>
                     </form>
                 </div>
